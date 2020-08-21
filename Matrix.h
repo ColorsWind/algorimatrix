c3e068285473b9ce1aaf6eddd8d2d81828b4bc02
colors_wind <1748591300@qq.com>
2020-08-21 09:43:17 +0800

    创建矩阵类头文件

--- Matrix.h
+++ Matrix.h
@@ -4,25 +4,25 @@
 
 #ifndef ALGORIMATRIX_MATRIX_H
 #define ALGORIMATRIX_MATRIX_H
-//
-// Created by colors_wind on 2020/7/22.
-//
-
-#ifndef ALGORIMATRIX_MATRIX_H
-#define ALGORIMATRIX_MATRIX_H
-
-#endif //ALGORIMATRIX_MATRIX_H
 
 class Matrix {
 private:
-    const int sizeM;
-    const int sizeN;
-    const int* array;
+    int m_row;
+    int m_col;
+    double* m_array;
 public:
-    Matrix(int number);
-    Matrix(int sizeM, int sizeN);
-    Matrix(int sizeM, int sizeN, int* array);
-    Materix friend operator+(Matrix m1, Matrix m2);
+    Matrix(int row, int col);
+    Matrix(int row, int col, double* array);
+    Matrix & operator+=(Matrix m);
+    Matrix & operator-=(Matrix m);
+    Matrix & operator*=(Matrix m);
+    Matrix & operator/=(Matrix m);
+    Matrix friend operator+(Matrix m1, Matrix m2);
+    Matrix friend operator-(Matrix m1, Matrix m2);
+    Matrix friend operator*(Matrix m1, Matrix m2);
+    Matrix friend operator/(Matrix m1, Matrix m2);
+    double & at(int row, int col);
+    int size();
 };
 
-#endif //ALGORIMATRIX_MATRIX_H
+#endif ALGORIMATRIX_MATRIX_H
