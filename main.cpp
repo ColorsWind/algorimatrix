ea4f6e89722d2dacee989b9399ad2c82319917ea
colors_wind <1748591300@qq.com>
2020-09-02 17:12:07 +0800

    添加测试

--- main.cpp
+++ main.cpp
@@ -7,20 +7,23 @@
 using namespace std;
 int main() {
     char a[100];
-    cin.getline(a, 100);
-    string command(a);
-    TokenStream tokenStream(command);
-    ExtendParser parser(tokenStream);
-    parser.advance();
-    Matrix matrix = parser.processE();
-    if (parser.getToken().getType() != END) {
-        cerr << "ERROR not end";
+    string command;
+    ExtendParser parser;
+    while(true) {
+        cin.getline(a, 100);
+        command = a;
+        parser.input(command);
+        Matrix matrix = parser.processS();
+        if (parser.getToken().getType() != END) {
+            cerr << "ERROR not end";
+        }
+        cout << "ANS = ";
+        if (matrix.size() > 1) {
+            cout << "\n";
+        }
+        cout << matrix.toString() << endl;
     }
-    cout << "ANS = ";
-    if (matrix.size() > 1) {
-        cout << "\n";
-    }
-    cout << matrix.toString() << endl;
+
 
 }
 
